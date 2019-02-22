68b4436c77bc81b83f4765f231c9508fb3db000b
Ishu Singh <ishusi@In-Ishusi-2.local>
2019-02-22 15:41:23 +0530

    Implemented move-left and move-right functions

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -1,15 +1,35 @@
 (ns twenty48.core
   (:gen-class))
 
+(def remove-zero (partial remove zero?))
+
+(def pair-of-two (partial partition-all 2))
+
+(def pair-consecutive-identical-elements (partial partition-by identity))
+
+(def add-elements-of-inner-array (partial map (partial apply +)))
+
+(def fill-zero-in-list (comp (partial take 4) (partial flatten) (partial conj (repeat 4 0))))
+
+(def move-left (comp (partial fill-zero-in-list)
+                     (partial add-elements-of-inner-array)
+                     (partial mapcat pair-of-two)
+                     (partial pair-consecutive-identical-elements)
+                     (partial remove-zero)))
+
+(def move-right (comp (partial reverse)
+                      (partial move-left)
+                      (partial reverse)))
+
 (defn move-grid-right
   "Moves an entire grid to the right"
   [grid]
-  grid)
+  (map move-right grid))
 
 (defn move-grid-left
   "Moves an entire grid to the left"
   [grid]
-  grid)
+  (map move-left grid))
 
 (defn move-grid-down
   "Moves an entire grid down"

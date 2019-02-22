526a15a995903217a794d09348c6f5f2d7a15f1b
Ishu Singh <ishusi@In-Ishusi-2.local>
2019-02-22 16:28:04 +0530

    Implemented move-up and move-down functions

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -11,6 +11,10 @@
 
 (def fill-zero-in-list (comp (partial take 4) (partial flatten) (partial conj (repeat 4 0))))
 
+(def transpose-grid (partial apply map vector))
+
+(transpose-grid `((2 3)(4 5)))
+
 (def move-left (comp (partial fill-zero-in-list)
                      (partial add-elements-of-inner-array)
                      (partial mapcat pair-of-two)
@@ -34,9 +38,13 @@
 (defn move-grid-down
   "Moves an entire grid down"
   [grid]
-  grid)
+  ((comp (partial transpose-grid)
+         (partial move-grid-right)
+         (partial transpose-grid)) grid ))
 
 (defn move-grid-up
   "Moves an entire grid up"
   [grid]
-  grid)
+  ((comp (partial transpose-grid)
+         (partial move-grid-left)
+         (partial transpose-grid)) grid))
